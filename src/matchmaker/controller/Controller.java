2d28abed4d50120dd031359dcba98c585e5ef190
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-10-16 09:01:04 -0600

    Created code for makeRecommendation method

--- src/matchmaker/controller/Controller.java
+++ src/matchmaker/controller/Controller.java
@@ -32,8 +32,50 @@ public class Controller {
    }
 
    private Book makeRecommendation(Student currentStudent, ArrayList<Book> bookList){
+      Book recommendedBook = null;
       ArrayList<Book> recommendList = new ArrayList<Book>();
 
-      return null;
+      for (int index = 0; index < bookList.size(); index++){
+         Book currentBook = bookList.get(index);
+         if (currentBook.getHasPictures() == currentStudent.getWantsPictures()){
+            recommendList.add(currentBook);
+         }
+      }
+
+      for (int index = recommendList.size() - 1; index >= 0; index--){
+         Book currentBook = recommendList.get(index);
+         if (currentBook.getIsInEnglish() != currentStudent.getIsEnglishSpeaker()){
+            recommendList.remove(index);
+         }
+      }
+
+      for (int index = recommendList.size() - 1; index >= 0; index--){
+         Book currentBook = recommendList.get(index);
+         String bookGenre = currentBook.getGenre();
+
+         if (!bookGenre.contains(currentStudent.getPreferredGenre())){
+            recommendList.remove(index);
+         }
+      }
+
+      for (int index = recommendList.size() - 1; index >= 0; index--){
+         Book currentBook = recommendList.get(index);
+         int readingLevel = currentBook.getDifficulty();
+         int range = readingLevel - (currentStudent.getAge() * 100);
+         range = Math.abs(range);
+
+         if (range <= 100){
+            recommendList.remove(index);
+         }
+      }
+
+      if (recommendList.size() > 0){
+         recommendedBook = recommendList.get(0);
+      } else {
+         int randomIndex = (int) Math.random() * bookList.size();
+         recommendedBook = recommendList.get(randomIndex);
+      }
+
+      return recommendedBook;
    }
 }
